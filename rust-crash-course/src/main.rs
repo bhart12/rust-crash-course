4f9ba8a5bed092ffb22c1842c333feee9ef90739
Bryce Hart <Bryce.Hart@jhuapl.edu>
2023-11-14 16:23:59 -0500

    End of Chapter 5

--- rust-crash-course/src/main.rs
+++ rust-crash-course/src/main.rs
@@ -14,6 +14,16 @@ fn empty_string(value: &mut String)
     value.clear();
 }
 
+fn say_hello_to_1(to_person: String) -> String
+{
+    format!("Hello, {}!", to_person)
+}
+
+fn process_name(name: &str, callback:fn(&str) -> ())
+{
+    callback(name);
+}
+
 fn main() {
     let mut name: &str = "John";
     name = "Jane";
@@ -28,6 +38,8 @@ fn main() {
     println!("Your tuple name is {}", tuple_data.0);
     println!("My const age {}", MY_AGE);
 
+
+    // Chapter 4
     let mut s1: String = String::from("James");
     // Create read only reference to s1
     let s2: &String = &s1;
@@ -36,4 +48,14 @@ fn main() {
     greet(s2);
     empty_string(&mut s1);
     greet(&s1);
+
+
+    // Chapter 5
+    println!("{}", say_hello_to_1(String::from("Jerry")));
+    // inline function
+    let say_hello_to_full_name = |first_name: &str, last_name: &str| format!("Hello, {} {}!", first_name, last_name);
+    println!("{}", say_hello_to_full_name("Jerry", "Johnson"));
+
+
+
 }
